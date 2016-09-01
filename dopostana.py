1597b09c40cc11336a8dad29b594afd2510cc7be
cpppy <1107706059@qq.com>
2016-09-01 16:24:19 +0800

    Update dopostana.py

--- dopostana.py
+++ dopostana.py
@@ -1,59 +1,97 @@
 # -*- coding: utf-8 -*-
-import readCSV
-import MySQLdb
 
-# read table in database , restore the data into dictionary 
-def table2list(tablename):
-	print "now , deal with table: ",tablename
-	print "try connecting the Database--'postana'--"
-	conn = MySQLdb.connect(host='localhost',user='root',passwd='ene',db='postana',charset='utf8')  
-	print  'DB is connected success !'
-	cur = conn.cursor() 
-	   
-	freq_arr=[]  
-	count = cur.execute('select frequency from '+tablename)	
-	results=cur.fetchall()
-	result=list(results)
-	for freq in result:
-		#print float(freq[0])
-		freq_arr.append(float(freq[0]))
-	cur.scroll(0,mode='absolute')  #index come to start point, default mode is "relative"
-	
-	word_arr=[]
-	count=cur.execute('select word from '+tablename)
-	results=cur.fetchall()
-	result=list(results)
-	for word in result:
-		#print word[0].encode('utf-8')
-		word_arr.append(word[0].encode('utf-8'))
-	
-	dic={}
-	print "word quantity=",len(word_arr),'\t','frequency quantity=',len(freq_arr)
-	for index in range(len(word_arr)):
-		dic[word_arr[index]]=freq_arr[index]
-		print word_arr[index],"   ",freq_arr[index]
-	
-	print "DATA in "+tablename+" have been written into Dictionary"
-	conn.close()
-	print 'DB --postana--   closed!'
-	print '----------------------------------------'
-	print '\n'
+import readDB2dic
+import getNew
+import getchlistfromhtml
+import collections
+
+# refresh the frequency of each word using idf_rates
+def multiIDF(dic,idf_dic):
+	for key,value in dic.items():
+		if key in idf_dic.keys():
+			if(idf_dic[key]<2.2):
+				idf_dic[key]=0
+			dic[key]=value*idf_dic[key]
+		else:
+			dic[key]=value*10
 	return dic
 
+#get new url resource from csdn bolg website
+def getWordFromNewUrl():
+	url_arr,urlname_dic = getNew.getCsdnUrlArr()    # !!! CSDN
+	wordsc = []
+	for url in url_arr:
+		wdlist = getchlistfromhtml.getChListFromHtml(url)
+		wordsc.append(wdlist)
+	return wordsc,url_arr,urlname_dic
+
+def wlist2ratesDic(wlist):
+	dic = {}
+	for word in wlist:
+		word = word.encode('utf-8')
+		if word not in dic.keys():
+			dic[word] = 0
+		dic[word] += 1
+	sum=len(wlist)
+	for key,value in dic.viewitems():
+		dic[key] = float(value)/sum
+	return dic
+
+# cosine similarity
+def calcScore(source_dic,new_dic):
+	score = 0
+	for key,value in new_dic.items():
+		if key in source_dic.keys():
+			score += value*source_dic[key]
+	return score
+
+
+
 if __name__ == '__main__':
-	csdn_dic = table2list('csdntable')
-	#zhihu_dic = table2list('zhihutable')
-	#baiduQA_dic = table2list('baiduQAtable')
 	
+	csdn_dic = readDB2dic.table2list('csdntable')
+	#zhihu_dic = readDB2dic.table2list('zhihutable')
+	#baiduQA_dic = readDB2dic.table2list('baiduQAtable')
+	idf_dic = readDB2dic.table2list('idftable')
+
+	csdn_dic = multiIDF(csdn_dic,idf_dic)
+	csdn_dic = collections.OrderedDict(sorted(csdn_dic.items(), key = lambda t: -t[1]))
+	print "csdn_dic------------****"
+	for key,value in csdn_dic.items():
+		print key,'\t',value
+		
 	
-
-
-
-
-
-
+	#get new word_bag from new url each day
+	wordsc,url_arr,urlname_dic = getWordFromNewUrl()
+	score_arr = []
+	for wlist in wordsc:
+		print 'get one wlist of a fresh url !'
+		new_dic = wlist2ratesDic(wlist)
+		new_dic = multiIDF(new_dic,idf_dic)
+		print 'get rates dic after multipuls the idf_frequency !' 
+		new_dic = collections.OrderedDict(sorted(new_dic.items(), key = lambda t: -t[1]))
+		print 'sort the new url wdlist dic after deal with by IDF algorithm !'
+		'''
+		print "new_dic------------****"
+		for key,value in new_dic.items():
+			print key,'\t',value
+		'''
+		score = calcScore(csdn_dic,new_dic)
+		score_arr.append(score)
 	
+	# restore score to dic with url
+	score_dic = {}
+	for i in range(len(score_arr)):
+		score_dic[url_arr[i]] = score_arr[i]
+		
+	score_dic = collections.OrderedDict(sorted(score_dic.items(), key = lambda t: -t[1]))
 	
-	
-	
+	# print results
+	for key,value in score_dic.items():
+		print 'Score = ',value
+		print 'URL   = ',key
+		print 'Title : ',urlname_dic[key]
+		print "----------------------------"
+		
+
 	
